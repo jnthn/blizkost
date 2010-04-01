d1f979cb3f45acadcd6a2f91fdb37cafd01358f1
Stefan O'Rear <stefanor@cox.net>
2010-03-31 22:50:02 -0700

    Add a Tk example (not yet working)

---
+++ examples/tk.pl
@@ -0,0 +1,20 @@
+# vim: ft=perl6
+# (actually NQP; the real Perl6 version will be much neater)
+
+pir::load_bytecode('perl5.pbc');
+my $comp := pir::compreg__PS('perl5');
+
+my $mod  := $comp.load_module('Tk');
+
+my %exp  := $comp.get_exports($mod);
+
+my $mw := %exp<sub><&MainWindow>().new;
+
+$mw.Label(-text => 'Hello, world!').pack;
+
+$mw.Button(
+    -text    => 'Quit',
+    -command => sub (*@_) { exit },
+).pack;
+
+
