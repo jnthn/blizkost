c26443721741cbd98befc372be25c4c46fef0706
Stefan O'Rear <stefanor@cox.net>
2010-04-25 02:23:18 -0700

    Update Tk example
    It now clearly delineates the 6 lines that are the responsibility of 'use' in
    a real HLL, from the lines which represent user code.

--- examples/tk.pl
+++ examples/tk.pl
@@ -1,14 +1,18 @@
 # vim: ft=perl6
 # (actually NQP; the real Perl6 version will be much neater)
 
+# next 6 lines would be 'use' in a real language
 pir::load_bytecode('perl5.pbc');
 my $comp := pir::compreg__ps('perl5');
 
 my $mod  := $comp.load_module('Tk');
-
 my %exp  := $comp.get_exports($mod);
 
-my $mw := $comp.eval("'MainWindow'").new;
+my $MainWindow := $comp.get_namespace('MainWindow');
+my &MainLoop := %exp<sub><MainLoop>;
+###
+
+my $mw := $MainWindow.new;
 
 $mw.Label('-text', 'Hello, world!').pack;
 
@@ -18,4 +22,4 @@ $mw.Button(
     '-command', sub (*@_) { exit },
 ).pack;
 
-%exp<sub><MainLoop>();
+&MainLoop();
