64df29fdc1c57a6f47e548bfc3e04ca6cd305090
Stefan O'Rear <stefanor@cox.net>
2010-04-30 00:39:20 -0700

    Reify nexuses as objects outside P5Interpreter
    This makes the code a bit more symmetric, makes the signatures a bit neater,
    provides a platform to avoid destruction order dependencies, and gives me an
    excuse to use the word nexus more.

--- src/pmc/bkmarshal.c
+++ src/pmc/bkmarshal.c
@@ -26,11 +26,11 @@ src/pmc/p5marshal.c - wrap P5 and Parrot calling conventions
 
 PARROT_WARN_UNUSED_RESULT
 PARROT_CANNOT_RETURN_NULL
-static CV *blizkost_wrap_callable(PARROT_INTERP, PMC *p5i, PMC *callable);
+static CV *blizkost_wrap_callable(BLIZKOST_NEXUS, PMC *callable);
 
 /*
 
-=item C<SV *blizkost_marshal_arg(PARROT_INTERP, PerlInterpreter *my_perl, PMC *arg)>
+=item C<SV *blizkost_marshal_arg(BLIZKOST_NEXUS, PMC *arg)>
 
 Takes a PMC and marshals it into an SV that we can pass to Perl 5.
 
@@ -41,11 +41,9 @@ Takes a PMC and marshals it into an SV that we can pass to Perl 5.
 PARROT_WARN_UNUSED_RESULT
 PARROT_CANNOT_RETURN_NULL
 SV *
-blizkost_marshal_arg(PARROT_INTERP, PMC *p5i, PMC *arg) {
+blizkost_marshal_arg(BLIZKOST_NEXUS, PMC *arg) {
     struct sv *result = NULL;
-    PerlInterpreter *my_perl;
-
-    GETATTR_P5Interpreter_my_perl(interp, p5i, my_perl);
+    dBNPERL; dBNINTERP;
 
     /* If it's a P5Scalar PMC, then we just fetch the SV from it - trivial
      * round-tripping. */
@@ -74,7 +72,7 @@ blizkost_marshal_arg(PARROT_INTERP, PMC *p5i, PMC *arg) {
         result = sv_2mortal(newSVpv(c_str, strlen(c_str)));
     }
     else if (VTABLE_isa(interp, arg, CONST_STRING(interp, "Sub"))) {
-        CV *wrapper = blizkost_wrap_callable(interp, p5i, arg);
+        CV *wrapper = blizkost_wrap_callable(nexus, arg);
         result = sv_2mortal(newRV_inc((SV*)wrapper));
     }
     else if ( VTABLE_does(interp, arg, CONST_STRING(interp, "array"))) {
@@ -84,7 +82,7 @@ blizkost_marshal_arg(PARROT_INTERP, PMC *p5i, PMC *arg) {
         while (VTABLE_get_bool(interp, iter)) {
              PMC *item = VTABLE_shift_pmc(interp, iter);
              struct sv *marshaled =
-                blizkost_marshal_arg(interp, p5i, item);
+                blizkost_marshal_arg(nexus, item);
              av_push( array, marshaled);
         }
         result = newRV_inc((SV*)array);
@@ -98,7 +96,7 @@ blizkost_marshal_arg(PARROT_INTERP, PMC *p5i, PMC *arg) {
         for(i = 0; i < n; i++) {
             STRING *s = VTABLE_shift_string(interp, iter);
             char *c_str = Parrot_str_to_cstring(interp, s);
-            struct sv *val = blizkost_marshal_arg(interp, p5i,
+            struct sv *val = blizkost_marshal_arg(nexus,
                     VTABLE_get_pmc_keyed_str(interp, arg, s));
             hv_store(hash, c_str, strlen(c_str), val, 0);
         }
@@ -124,16 +122,15 @@ the SV's reference count.
 PARROT_WARN_UNUSED_RESULT
 PARROT_CANNOT_RETURN_NULL
 PMC *
-blizkost_wrap_sv(PARROT_INTERP, PMC *p5i, SV *sv) {
-    PerlInterpreter *my_perl;
+blizkost_wrap_sv(BLIZKOST_NEXUS, SV *sv) {
+    dBNPERL; dBNINTERP;
     PMC *wrapper = Parrot_pmc_new_noinit(interp, pmc_type(interp,
                 string_from_literal(interp, "P5Scalar")));
 
     PObj_custom_mark_SET(wrapper);
     PObj_custom_destroy_SET(wrapper);
 
-    GETATTR_P5Interpreter_my_perl(interp, p5i, my_perl);
-    SETATTR_P5Scalar_p5i(interp, wrapper, p5i);
+    SETATTR_P5Scalar_nexus(interp, wrapper, nexus);
     SETATTR_P5Scalar_sv(interp, wrapper, SvREFCNT_inc(sv));
     return wrapper;
 }
@@ -171,11 +168,10 @@ blizkost_return_from_invoke(PARROT_INTERP, void *next) {
 }
 
 int
-blizkost_slurpy_to_stack(PARROT_INTERP, PMC *p5i, PerlInterpreter *my_perl,
-        PMC *positional, PMC *named) {
+blizkost_slurpy_to_stack(BLIZKOST_NEXUS, PMC *positional, PMC *named) {
     int num_pos, i, stkdepth;
     PMC *iter;
-    dSP;
+    dBNPERL; dBNINTERP; dSP;
 
     stkdepth = 0;
 
@@ -183,7 +179,7 @@ blizkost_slurpy_to_stack(PARROT_INTERP, PMC *p5i, PerlInterpreter *my_perl,
     num_pos = VTABLE_elements(interp, positional);
     for (i = 0; i < num_pos; i++) {
         PMC *pos_arg = VTABLE_get_pmc_keyed_int(interp, positional, i);
-        XPUSHs(blizkost_marshal_arg(interp, p5i, pos_arg));
+        XPUSHs(blizkost_marshal_arg(nexus, pos_arg));
         stkdepth++;
     }
 
@@ -195,7 +191,7 @@ blizkost_slurpy_to_stack(PARROT_INTERP, PMC *p5i, PerlInterpreter *my_perl,
         PMC    *arg_value  = VTABLE_get_pmc_keyed_str(interp, named, arg_name);
         char   *c_arg_name = Parrot_str_to_cstring(interp, arg_name);
         XPUSHs(sv_2mortal(newSVpv(c_arg_name, strlen(c_arg_name))));
-        XPUSHs(blizkost_marshal_arg(interp, p5i, arg_value));
+        XPUSHs(blizkost_marshal_arg(nexus, arg_value));
         stkdepth += 2;
     }
     PUTBACK;
@@ -203,12 +199,10 @@ blizkost_slurpy_to_stack(PARROT_INTERP, PMC *p5i, PerlInterpreter *my_perl,
 }
 
 void
-blizkost_call_in(PARROT_INTERP, PMC *p5i, SV *what, U32 mode, PMC *positp,
-        PMC *namedp, PMC **retp) {
-    PerlInterpreter *my_perl;
+blizkost_call_in(BLIZKOST_NEXUS, SV *what, U32 mode, PMC *positp, PMC *namedp,
+        PMC **retp) {
+    dBNPERL; dBNINTERP;
     int num_returns, i;
-
-    GETATTR_P5Interpreter_my_perl(interp, p5i, my_perl);
 
     {
         /* Set up the stack. */
@@ -218,7 +212,7 @@ blizkost_call_in(PARROT_INTERP, PMC *p5i, SV *what, U32 mode, PMC *positp,
         PUSHMARK(SP);
 
         PUTBACK;
-        blizkost_slurpy_to_stack(interp, p5i, my_perl, positp, namedp);
+        blizkost_slurpy_to_stack(nexus, positp, namedp);
 
         /* Invoke the methods. */
         num_returns = call_sv(what, mode);
@@ -228,7 +222,7 @@ blizkost_call_in(PARROT_INTERP, PMC *p5i, SV *what, U32 mode, PMC *positp,
         *retp = pmc_new(interp, enum_class_ResizablePMCArray);
         for (i = 0; i < num_returns; i++) {
             SV *result_sv = POPs;
-            PMC *result_pmc = blizkost_wrap_sv(interp, p5i, result_sv);
+            PMC *result_pmc = blizkost_wrap_sv(nexus, result_sv);
             VTABLE_unshift_pmc(interp, *retp, result_pmc);
         }
         PUTBACK;
@@ -255,11 +249,9 @@ for use in callbacks from Perl 5, it throws a Perl 5 exception on failure.
 static int
 blizkost_delete_binding(PerlInterpreter *my_perl, SV *handle, MAGIC *mg)
 {
-    Parrot_P5Interpreter_attributes *back
-        = (Parrot_P5Interpreter_attributes *)(mg->mg_ptr);
-
+    blizkost_nexus *nexus = (blizkost_nexus *)(mg->mg_ptr);
+    dBNINTERP;
     PMC *targ = (PMC *)(mg->mg_obj);
-    Parrot_Interp interp = back->parrot_interp;
 
     PARROT_CALLIN_START(interp);
     Parrot_pmc_gc_unregister(interp, targ);
@@ -272,10 +264,9 @@ static MGVTBL blizkost_binder_vtbl = { 0, 0, 0, 0, blizkost_delete_binding,
     0, 0, 0 };
 
 void
-blizkost_get_bound_pmc(PerlInterpreter *my_perl, SV *sv,
-        Parrot_Interp *interpr, PMC **p5ir, PMC **targetr) {
+blizkost_get_bound_pmc(PerlInterpreter *my_perl,
+        blizkost_nexus **nexusr, SV *sv, PMC **targetr) {
     MAGIC *mgp;
-    Parrot_P5Interpreter_attributes *back;
 
     if (SvMAGICAL(sv))
         for (mgp = SvMAGIC(sv); mgp; mgp = mgp->mg_moremagic)
@@ -286,20 +277,18 @@ blizkost_get_bound_pmc(PerlInterpreter *my_perl, SV *sv,
 
 gotmagic:
 
-    back = (Parrot_P5Interpreter_attributes *)(mgp->mg_ptr);
-
-    *interpr = back->parrot_interp;
-    *p5ir    = back->self;
+    *nexusr = (blizkost_nexus *)(mgp->mg_ptr);
     *targetr = (PMC *)mgp->mg_obj;
 }
 
 void
-blizkost_bind_pmc_to_sv(PerlInterpreter *my_perl, SV *sv,
-        PARROT_INTERP, PMC *p5i, PMC *target) {
+blizkost_bind_pmc_to_sv(BLIZKOST_NEXUS, SV *sv, PMC *target) {
+    dBNPERL; dBNINTERP;
+
     MAGIC *mg;
 
     mg = sv_magicext(sv, 0, PERL_MAGIC_ext, &blizkost_binder_vtbl, 0, 0);
-    mg->mg_ptr = (char*)PARROT_P5INTERPRETER(p5i);
+    mg->mg_ptr = (char*)nexus;
     mg->mg_obj = (SV*)  target;
 
     Parrot_pmc_gc_register(interp, target);
@@ -313,28 +302,28 @@ XS(blizkost_callable_trampoline)
     dVAR;
 #endif
     dXSARGS;
-    PMC *callable, *p5i;
-    Parrot_Interp interp;
+    PMC *callable;
+    blizkost_nexus *nexus;
     int i;
     PMC *args, *posret, *namret;
 
-    blizkost_get_bound_pmc(my_perl, (SV *)cv, &interp, &p5i, &callable);
+    blizkost_get_bound_pmc(my_perl, &nexus, (SV *)cv, &callable);
 
     PERL_UNUSED_VAR(ax);
     SP -= items;
     PUTBACK;
 
-    args = Parrot_pmc_new(interp, enum_class_ResizablePMCArray);
+    args = Parrot_pmc_new(nexus->parrot_interp, enum_class_ResizablePMCArray);
     for (i = 0; i < items; i++) {
         SV *svarg = ST(i);
-        PMC *pmcarg = blizkost_wrap_sv(interp, p5i, svarg);
-        VTABLE_unshift_pmc(interp, args, pmcarg);
+        PMC *pmcarg = blizkost_wrap_sv(nexus, svarg);
+        VTABLE_unshift_pmc(nexus->parrot_interp, args, pmcarg);
     }
 
-    Parrot_pcc_invoke_sub_from_c_args(interp, callable, "Pf->PsPsn",
-            args, &posret, &namret);
+    Parrot_pcc_invoke_sub_from_c_args(nexus->parrot_interp, callable,
+            "Pf->PsPsn", args, &posret, &namret);
 
-    blizkost_slurpy_to_stack(interp, p5i, my_perl, posret, namret);
+    blizkost_slurpy_to_stack(nexus, posret, namret);
 
     SPAGAIN;
 }
@@ -342,15 +331,13 @@ XS(blizkost_callable_trampoline)
 PARROT_WARN_UNUSED_RESULT
 PARROT_CANNOT_RETURN_NULL
 static CV *
-blizkost_wrap_callable(PARROT_INTERP, PMC *p5i, PMC *callable) {
-    PerlInterpreter *my_perl;
+blizkost_wrap_callable(BLIZKOST_NEXUS, PMC *callable) {
+    dBNPERL;
     CV *cv;
-
-    GETATTR_P5Interpreter_my_perl(interp, p5i, my_perl);
 
     cv = newXS("blizkost_xs_wrapper", blizkost_callable_trampoline,
             "bkmarshal.c");
-    blizkost_bind_pmc_to_sv(my_perl, (SV*)cv, interp, p5i, callable);
+    blizkost_bind_pmc_to_sv(nexus, (SV*)cv, callable);
 
     return cv;
 }
--- src/pmc/bkmarshal.h
+++ src/pmc/bkmarshal.h
@@ -17,28 +17,40 @@ src/pmc/bkmarshal.h - wrap P5 and Parrot calling conventions
 #ifndef BLIZKOST_MARSHAL_H_GUARD
 #define BLIZKOST_MARSHAL_H_GUARD
 
-PARROT_WARN_UNUSED_RESULT
-PARROT_CANNOT_RETURN_NULL
-SV *blizkost_marshal_arg(PARROT_INTERP, PMC *p5i, PMC *arg);
+/* Holds all data which ties Perl5 and Parrot together.  We can't store it in
+   either's heap without destruction order fiascos. */
+typedef struct blizkost_nexus {
+    PerlInterpreter *my_perl;
+    Parrot_Interp parrot_interp;
+    PMC *p5i;
+} blizkost_nexus;
+
+#define BLIZKOST_NEXUS blizkost_nexus *nexus
+#define BNINTERP (nexus->parrot_interp)
+#define dBNPERL PerlInterpreter *const my_perl = nexus->my_perl
+#define iBNPERL my_perl = nexus->my_perl
+#define dBNINTERP Parrot_Interp const interp = nexus->parrot_interp
 
 PARROT_WARN_UNUSED_RESULT
 PARROT_CANNOT_RETURN_NULL
-PMC *blizkost_wrap_sv(PARROT_INTERP, PMC *p5i, SV *sv);
+SV *blizkost_marshal_arg(BLIZKOST_NEXUS, PMC *arg);
+
+PARROT_WARN_UNUSED_RESULT
+PARROT_CANNOT_RETURN_NULL
+PMC *blizkost_wrap_sv(BLIZKOST_NEXUS, SV *sv);
 
 PARROT_WARN_UNUSED_RESULT
 PARROT_CANNOT_RETURN_NULL
 opcode_t *blizkost_return_from_invoke(PARROT_INTERP, void *next);
 
-int blizkost_slurpy_to_stack(PARROT_INTERP, PMC *p5i, PerlInterpreter *my_perl,
-        PMC *positional, PMC *named);
+int blizkost_slurpy_to_stack(BLIZKOST_NEXUS, PMC *positional, PMC *named);
 
-void blizkost_call_in(PARROT_INTERP, PMC *p5i, SV *what, U32 mode,
-        PMC *positp, PMC *namedp, PMC **retp);
+void blizkost_call_in(BLIZKOST_NEXUS, SV *what, U32 mode, PMC *positp,
+        PMC *namedp, PMC **retp);
 
-void blizkost_bind_pmc_to_sv(PerlInterpreter *my_perl, SV *sv,
-        PARROT_INTERP, PMC *p5i, PMC *target);
+void blizkost_bind_pmc_to_sv(BLIZKOST_NEXUS, SV *sv, PMC *target);
 
-void blizkost_get_bound_pmc(PerlInterpreter *my_perl, SV *sv,
-        Parrot_Interp *interpr, PMC **p5ir, PMC **targetr);
+void blizkost_get_bound_pmc(PerlInterpreter *my_perl,
+        blizkost_nexus **nexusr, SV *sv, PMC **targetr);
 
 #endif
